1af1ea30e71937682502764c69344a5cc527cdc4
周文 <chowenzhou@gmail.com>
2017-03-01 17:35:25 +0800

    test recyclerview

--- app/src/main/java/com/chowen/apackage/testkitdemo/dianshang_home_page/recyclerview/HomeRecyclerViewPage.java
+++ app/src/main/java/com/chowen/apackage/testkitdemo/dianshang_home_page/recyclerview/HomeRecyclerViewPage.java
@@ -14,9 +14,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chowen.apackage.testkitdemo.R;
 import com.chowen.apackage.testkitdemo.dianshang_home_page.adapter.HomeListAdapter;
+import com.chowen.apackage.testkitdemo.utils.L;
 import com.chowen.apackage.testkitdemo.viewpage.base.adapter.ViewPageAdapter;
 
 import java.util.ArrayList;
@@ -75,6 +77,8 @@ public class HomeRecyclerViewPage extends SupportFragment {
         }
 
         mAdapter = new ImageAdapter(list);
+        mAdapter.setHeaderView(mHeaderView);
+
 
         mRecyclerList = (RecyclerView) mView.findViewById(R.id.recycler_list);
 //        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
@@ -84,10 +88,20 @@ public class HomeRecyclerViewPage extends SupportFragment {
         mRecyclerList.setItemAnimator(new DefaultItemAnimator());
         mRecyclerList.setAdapter(mAdapter);
 
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position == 0) {
+                    return 2;
+                }
+                return 1;
+            }
+        });
+
     }
 
     private void initViews() {
-        mHeaderView = LayoutInflater.from(getContext()).inflate(R.layout.header_layout, null);
+        mHeaderView = LayoutInflater.from(getContext()).inflate(R.layout.header_layout, null, false);
     }
 
     private void initViewPager() {
@@ -96,7 +110,7 @@ public class HomeRecyclerViewPage extends SupportFragment {
         mList.add(getActivity().getLayoutInflater().inflate(R.layout.home_view_page, null));
         mList.add(getActivity().getLayoutInflater().inflate(R.layout.home_view_page, null));
 
-        mViewPager = (ViewPager) mView.findViewById(R.id.view_pager);
+        mViewPager = (ViewPager) mHeaderView.findViewById(R.id.view_pager);
 
         mViewPageAdapter = new ViewPageAdapter(mList);
         mViewPager.setAdapter(mViewPageAdapter);
@@ -132,10 +146,22 @@ public class HomeRecyclerViewPage extends SupportFragment {
         }
 
         @Override
+        public int getItemViewType(int position) {
+            if (position == 0) {
+                return VIEW_HEADER;
+            }
+            return VIEW_ITEM;
+        }
+
+        @Override
         public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == VIEW_HEADER) {
+                return new ImageAdapter.ViewHolder(headerView);
+            } else {
             View v = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.recycler_item_view, parent, false);
             return new ImageAdapter.ViewHolder(v);
+            }
         }
 
 
@@ -160,7 +186,8 @@ public class HomeRecyclerViewPage extends SupportFragment {
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //showToast("This is RecyclerView Item");
+                        Toast.makeText(v.getContext(), "itemView"+v.getId(), Toast.LENGTH_LONG).show();
+                        L.e("This is RecyclerView Item");
                     }
                 });
             }
